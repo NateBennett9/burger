bad97bc7629f292f494392b072ada99d54e157fe
Nate Bennett <26212562+NateBennett9@users.noreply.github.com>
2019-07-02 15:31:35 -0700

    skeleton folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+SELECT * FROM burgers_db.burgers;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mondo Burger", false);
